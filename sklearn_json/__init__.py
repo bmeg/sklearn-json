c0928b7e04242d39f668be199ce439326abd1979
Jeena Lee <ijeenalee@gmail.com>
2016-04-26 10:00:08 -0700

    initial commit

---
+++ sklearn_json/__init__.py
@@ -0,0 +1,1 @@
+import sklearn_json.extensions
---
+++ sklearn_json/extensions/__init__.py
@@ -0,0 +1,4 @@
+import sklearn_json.extensions.linear_regression
+import sklearn_json.extensions.linearSVC
+import sklearn_json.extensions.logistic_regression
+
